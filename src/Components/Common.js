32c1ad2674ab2d9b238373f96c5a986b93f095dd
Umesh Pal <44876181+umesh996738@users.noreply.github.com>
2025-01-12 07:37:04 +0530

    Update Common.js
    summary

--- src/Components/Common.js
+++ src/Components/Common.js
@@ -23,9 +23,7 @@ const Common = (props) => {
                                  i'm a Software Engineer Makes Society Better.
                             </h2>
                             <p> 
-                                Results-driven Java Developer with 1.6 years of experience in designing, developing,
-                                and maintaining applications. Proficient in Java and related technologies, with a strong 
-                                foundation in software development principles and practices. Adept at collaborating in team 
+                                Results-driven designing, developing,and maintaining applications.Adept at collaborating in team 
                                 environments to deliver high-quality software solutions.
                             </p>
                             <div className="mt-3">
